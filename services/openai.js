21912f849df4324a94f1138b5838ce325cb63eef
oxfoxlion <oxfoxlion@gmail.com>
2025-06-12 22:55:33 +0800

    改回gpt-3.5-turbo

--- services/openai.js
+++ services/openai.js
@@ -10,7 +10,7 @@ export async function askChatGPT(messages) {
 
 
     const completion = await openai.chat.completions.create({
-        model: 'gpt-4o-mini',
+        model: 'gpt-3.5-turbo',
         messages: [
             { role: 'system', content:"請以繁體中文對話"},
             ...messages,
